7b230248d7b6178e6a68367089dbbdd5539ed66a
Zlegend27 <Zaid.H.Hammad@gmail.com>
2024-01-16 02:11:01 -0600

    comming cpress project

--- cypress/e2e/16-Assertions.cy.js
+++ cypress/e2e/16-Assertions.cy.js
@@ -4,22 +4,22 @@ describe("Assertions", () => {
 
     beforeEach(() => {
         // This will fail if the page doesnt send 
-      cy.visit("https://techglobal-training.com/frontend/");
-      cy.get(".cards").contains("Html Elements").click();
+        cy.visit("https://techglobal-training.com/frontend/");
+        cy.get(".cards").contains("Html Elements").click();
     });
 
     it("Default Assertions", () => {
         cy
-        // there is a defualt assertion that thi
-        // button must exist in the DOM before processing
-        .get('#register_buttom')
-        // since cypress internally checks if the web element on the dom tree or not
-        // we dont need to below assertion
-        // .should('be.visible')
+            // there is a defualt assertion that thi
+            // button must exist in the DOM before processing
+            .get('#register_buttom')
+            // since cypress internally checks if the web element on the dom tree or not
+            // we dont need to below assertion
+            // .should('be.visible')
 
-        // before issuing the click, this button must be "actionable"
-        // it cannot be covered, or disabled, or hidden from the view
-        .click()
+            // before issuing the click, this button must be "actionable"
+            // it cannot be covered, or disabled, or hidden from the view
+            .click()
 
         cy.get('#date_input1').click()
         // this will fail necause it wil; be covered by input 1
@@ -56,7 +56,7 @@ describe("Assertions", () => {
 
         cy.get('#main_heading').should('have.css', 'color', 'rgb(105, 105, 105)')
         cy.get('#main_heading').should('have.css', 'padding', '')
-        
+
 
     })
     it('add product', () => {
@@ -65,8 +65,8 @@ describe("Assertions", () => {
         cy.get('.delete').click()
 
         cy.get('.modal-card')
-        // do not use this if you want to validate and its not visible anymore
-        .should('not.exist')
+            // do not use this if you want to validate and its not visible anymore
+            .should('not.exist')
 
         cy.get('#main_heading').invoke('text').then((element) => {
             const el = element
@@ -93,10 +93,10 @@ describe("Assertions", () => {
 
         cy.get('[data-identifier="Paragraphs"] > p').then(($el) => {
             // cy.log($el.length)
-    
+
             expect($el).to.have.length(2)
         })
-    
+
         cy.get('#checkbox_1').then(($el) => {
             expect($el).to.be.enabled
             expect($el).to.be.visible
@@ -107,17 +107,256 @@ describe("Assertions", () => {
         cy.get('#main_heading').then(($el) => {
             expect($el.css('color')).to.equal('rgb(105, 105, 105')
         })
-        
+
     })
 
     it('Validate multiple elements', () => {
         cy.get('#hello_paragraph').should('be.visible').and('have.text', 'Hello World!')
         cy.get('#testing_paragraph').should('be.visible').and('have.text', 'I like automation testing!')
 
-        cy.get('[data-identifier')
+        /**
+   * 1. Navigate to "https://techglobal-training.com/frontend/project-4"
+   * 2. Click on Add Product Button > Model Opens
+   * 3. Close the model and validate model is not visible
+   */
+
+        cy.visit('https://techglobal-training.com/frontend/project-4')
+
+        cy.get('#add_product_btn').click()
+        cy.get('.delete').click()
+
+        cy.get('.modal-card')
+            // do not use this if you want to validate its not visible anymroe
+            // .should('not.be.visible')
+            // ** instead, use this **
+            .should('not.exist')
     })
 
-    
-})
+    it('Explicit Assertons', () => {
 
-   
+        // This is explicit assertion
+        expect(true).equal(true)
+
+        // this is a failure
+        // expect(cy.url()).equal(cy.url())
+
+        cy.get('#main_heading').should('have.text', 'Html Elements')
+
+        const el = cy.get('#main_heading')
+
+        el.should('have.text', 'Html Elements')
+
+
+
+        cy.get('#main_heading').invoke('text').then((element) => {
+            const el = element
+            cy.log(el.toUpperCase() + ' retrieved element')
+
+            expect(el).equal('Html Elements')
+        })
+
+
+        cy.get('#main_heading').then(($ele) => {
+            const el = $ele.text()
+            cy.log(el.toUpperCase())
+            expect(el).to.equal('Html Elements')
+            expect(el).to.include('Html Elements')
+            expect(el).to.exist
+        })
+
+
+        cy.get('#main_heading').invoke('attr', 'id').then((attr) => {
+            expect(attr).to.equal('main_heading')
+        })
+
+        cy.get('#main_heading').then(($el) => {
+            const attr = $el.attr('id')
+            expect(attr).to.equal('main_heading')
+        })
+
+
+        cy.get('[data-identifier="Paragraphs"] > p').then(($el) => {
+            // cy.log($el.length)
+
+            expect($el).to.have.length(2)
+        })
+
+        cy.get('#checkbox_1').then(($el) => {
+            expect($el).to.be.enabled
+            expect($el).to.be.visible
+            expect($el).not.to.be.checked
+        })
+
+        // cy.get('#main_heading').should('have.css', 'color', 'rgb(105, 105, 105)')
+
+
+        cy.get('#main_heading').then(($el) => {
+            expect($el.css('color')).to.equal('rgb(105, 105, 105)')
+        })
+    })
+
+    it('Validate Multiple Elements', () => {
+
+        cy.get('#hello_paragraph').should('be.visible').and('have.text', 'Hello World!')
+        cy.get('#testing_paragraph').should('be.visible').and('have.text', 'I like automation testing!')
+
+        cy.get('[data-identifier="Paragraphs"] > p').as('paragraps')
+
+        const arr = ['Hello World!', 'I like automation testing!']
+
+        for (let i = 0; i < arr.length; i++) {
+            cy.get('@paragraps').eq(i).should('have.text', arr[i])
+        }
+
+        cy.get('@paragraps').each(($el, index) => {
+            // cy.log($el.text() + `${index}. of the web element`)
+            expect($el.text()).to.equal(arr[index])
+            expect($el).to.be.visible
+        })
+    })
+    it('Explicit Assertons', () => {
+
+        // This is explicit assertion
+        expect(true).equal(true)
+
+        // this is a failure
+        // expect(cy.url()).equal(cy.url())
+
+        cy.get('#main_heading').should('have.text', 'Html Elements')
+
+        const el = cy.get('#main_heading')
+
+        el.should('have.text', 'Html Elements')
+
+
+
+        cy.get('#main_heading').invoke('text').then((element) => {
+            const el = element
+            cy.log(el.toUpperCase() + ' retrieved element')
+
+            expect(el).equal('Html Elements')
+        })
+
+
+        cy.get('#main_heading').then(($ele) => {
+            const el = $ele.text()
+            cy.log(el.toUpperCase())
+            expect(el).to.equal('Html Elements')
+            expect(el).to.include('Html Elements')
+            expect(el).to.exist
+        })
+
+
+        cy.get('#main_heading').invoke('attr', 'id').then((attr) => {
+            expect(attr).to.equal('main_heading')
+        })
+
+        cy.get('#main_heading').then(($el) => {
+            const attr = $el.attr('id')
+            expect(attr).to.equal('main_heading')
+        })
+
+
+        cy.get('[data-identifier="Paragraphs"] > p').then(($el) => {
+            // cy.log($el.length)
+
+            expect($el).to.have.length(2)
+        })
+
+        cy.get('#checkbox_1').then(($el) => {
+            expect($el).to.be.enabled
+            expect($el).to.be.visible
+            expect($el).not.to.be.checked
+        })
+
+        // cy.get('#main_heading').should('have.css', 'color', 'rgb(105, 105, 105)')
+
+
+        cy.get('#main_heading').then(($el) => {
+            expect($el.css('color')).to.equal('rgb(105, 105, 105)')
+        })
+    })
+
+    it('Validate Multiple Elements', () => {
+
+        cy.get('#hello_paragraph').should('be.visible').and('have.text', 'Hello World!')
+        cy.get('#testing_paragraph').should('be.visible').and('have.text', 'I like automation testing!')
+
+        cy.get('[data-identifier="Paragraphs"] > p').as('paragraps')
+
+        const arr = ['Hello World!', 'I like automation testing!']
+
+        for (let i = 0; i < arr.length; i++) {
+            cy.get('@paragraps').eq(i).should('have.text', arr[i])
+        }
+
+        cy.get('@paragraps').each(($el, index) => {
+            // cy.log($el.text() + `${index}. of the web element`)
+            expect($el.text()).to.equal(arr[index])
+            expect($el).to.be.visible
+        })
+
+        /**
+        * 1. On the Html Elements page
+        * 2. From the "Headings" section, locate both "Programming Languages" and "Automation Tools" web elements
+        * 3. Validate their texts with expected text
+        * 4. Validate these elements are visible.
+        */
+
+        const headingTexts = ['Programming Languages', 'Automation Tools']
+
+        cy.get('[data-identifier="Headings"] > h4').each(($el, index) => {
+            cy.wrap($el).should('have.text', headingTexts[index]).and('be.visible')
+        })
+
+        /**
+         * 1. On the Html Elements Page
+         * 2.
+         */
+
+        const checkBoxTexts = ['Apple', 'Microsoft', 'Tesla']
+
+        cy.get('#checkbox-button-group div').each(($el, index) => {
+
+            cy.wrap($el).find('label').should('have.text', checkBoxTexts[index])
+            .parent().find('input').should('be.visible').and('be.enabled')
+        })
+    })
+
+       /**
+         * 1. Go to https://techglobal-training.com/frontend
+         * 2. Navigate to 'Html Elements' card
+         * 3. From the "Text Inputs" section
+         * 4. Validate text input 1 and text input 2 is enabled
+         * 5. Validate text input 1 and text input 2 is is not required
+        * 6. Enter your name and last name
+        */
+
+       it('Validating more stuff', () => {
+            const names = ['Zaid', 'Hammad']
+
+            cy.get('[data-identifier="Text Inputs"] input').each(($el, index) => {
+            cy.wrap($el).type(names[index]).should('be.enabled').and('not.have.attr', 'required')
+
+            })
+       })
+
+       it('Validating even more stuff', () => {
+        const dates = ['10/20/2017', '01/22/2023']
+
+        cy.get('data-identifier="Date Inputs"] input').each(($el, index) => {
+        cy.wrap($el).clear().type(`${dates[index]}{enter}`)
+        .should('have.value', dates[index])
+        .and('be.enabled')
+        .and('not.have.attr', 'required')
+        })
+   })
+
+   it('wow', () => {
+    const options = ['Apple', 'Microsoft']
+
+    cy.get('#company_dropdown1, #company_dropdown2').each(($el, index) => {
+        cy.wrap($el).select(options[index]).should('be.enabled').and('have.value', options[index])
+    })
+})
+})
--- cypress/e2e/Project1/form-elements-project.cy.js
+++ cypress/e2e/Project1/form-elements-project.cy.js
@@ -5,7 +5,116 @@ describe('From Elements Tests', () => {
         cy.visit('https://techglobal-training.com/frontend/project-1')
     })
 
-    it('Test Case 01', () => {
+    it('Test Case 01 | Validate the Contact Us information', () => {
+
+        // Validate the heading is “Contact Us”
+        cy.get('.is-size-3').should('have.text', 'Contact Us')
+
+        // Validate the address is “2800 S River Rd Suite 310, Des Plaines, IL 60018”
+        cy.get('#address').should('have.text', '2800 S River Rd Suite 310, Des Plaines, IL 60018')
+
+        // Validate the email is “info@techglobalschool.com"
+        cy.get('#email').should('have.text', 'info@techglobalschool.com')
+
+        // Validate the phone number is “(224) 580-2150”
+        cy.get('#phone-number').should('have.text', '(224) 580-2150')
+    })
+
+    it('Test Case 02 | Validate the Full name input box', () => {
+
+        // Validate that the Full name input box is displayed
+        // Validate that the Full name input box is required
+        // Validate that the placeholder of the Full name input box is “Enter your full name”
+        cy.get(':nth-child(1) > .control > .input').should('have.attr', 'placeholder', 'Enter your full name').and('have.attr', 'required')
+
+        // Validate that the label of the Full name input box is “Full name *”
+        cy.get('form > :nth-child(1) > .label').should('have.text', 'Full name *')
+    })
+
+
+    it('Test Case 03 - Validate the Gender radio button', () => {
+        // Validate the label is “Gender *”
+        cy.get('.control > .label').should('have.text', 'Gender *')
+
+        // Validate that the Gender is required
         
+        cy.get('.mr-1').first().should('be.enabled').and('have.attr', 'required')
+        
+
+        // Validate the options are “Female”, “Male” and “Prefer not to disclose”
+        cy.get('.radio').first().should('have.text', 'Male')
+        .next().should('have.text', 'Female')
+        .next().should('have.text', 'Prefer not to disclose')
+
+        // Validate the options are clickable and not selected
+        cy.get('.mr-1').should('be.enabled').and('not.be.checked')
+        
+        // Click on the “Male” option and validate it is selected while the others are not selected
+        cy.get('.mr-1').first().click().should('be.checked')
+        cy.get(':nth-child(3) > .mr-1').should('not.be.checked')
+        cy.get('.mr-1').last().should('not.be.checked')
+
+        // Click on the “Female” option and validate it is selected while the others are not selected
+        cy.get(':nth-child(3) > .mr-1').click().should('be.checked')
+        cy.get('.mr-1').first().should('not.be.checked')
+        cy.get('.mr-1').last().should('not.be.checked')
+    })
+
+    it('Test Case 04 | Validate the Address input box', () => {
+
+        // Validate that the Address input box is displayed
+        // Validate that the Address input box is not required
+        // Validate that the placeholder of the Address input box is “Enter your address*”
+        cy.get(':nth-child(3) > .control > .input').should('have.attr', 'placeholder', 'Enter your address').and('not.have.attr', 'required')
+        
+        // Validate that the label of the Address input box is “Address”
+        cy.get(':nth-child(3) > .label').should('have.text', 'Address')
+    })
+
+    it('Test Case 05 | Validate the Email input box', () => {
+
+        // Validate that the Email input box is displayed
+        // Validate that the Email input box is required
+        // Validate that the placeholder of the Email input box is “Enter your email”
+        cy.get(':nth-child(4) > .control > .input').should('have.attr', 'placeholder', 'Enter your email').and('have.attr', 'required')
+
+        // Validate that the label of the Email input box is “Email *”
+        cy.get(':nth-child(4) > .label').should('have.text', 'Email *')
+    })
+
+    it('Test Case 06 | Validate the Phone input box', () => {
+
+        // Validate that the Phone input box is displayed
+        // Validate that the Phone input box is not required
+        // Validate that the placeholder of the Address input box is “Enter your phone number”
+        cy.get(':nth-child(5) > .control > .input').should('have.attr', 'placeholder', 'Enter your phone number').and('not.have.attr', 'required')
+
+        // Validate that the label of the Phone input box is “Phone”
+        cy.get(':nth-child(5) > .label').should('have.text', 'Phone')
+    })
+
+    it('Test Case 07 | Validate the Message text area', () => {
+
+        // Validate that the Message text area is displayed
+        // Validate that the Message text area is not required
+        // Validate that the placeholder of the Message text area is “Type your message here…”
+        cy.get('.textarea').should('have.attr', 'placeholder', 'Type your message here...').and('not.have.attr', 'required')
+
+        // Validate that the label of the Message text area is “Message”
+        cy.get(':nth-child(6) > .label').should('have.text', 'Message')
+    })
+
+    it('Test Case 08 - Validate the Consent checkbox', () => {
+
+        // Validate the label is “I give my consent to be contacted.”
+        cy.get('.checkbox').should('have.text', ' I give my consent to be contacted.')
+        
+        // Validate that the Consent checkbox is required
+        // Validate that the Consent checkbox is clickable
+        // Click on the “I give my consent to be contacted.” checkbox and validate it is selected
+        cy.get('.checkbox > input').click().should('be.checked').and('be.enabled').and('have.attr', 'required')
+        
+        // Click on the “I give my consent to be contacted.” checkbox again and validate it is not selected
+        cy.get('.checkbox > input').click().should('not.be.checked')
     })
 })
